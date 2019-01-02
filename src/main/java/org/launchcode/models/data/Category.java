ada7a38d625fdfd82b2c35fee8e16b8a7582aa50
CordyRichardson <cordyrichardson3@gmail.com>
2019-01-01 22:51:52 -0500

    assignment part 2 one-to-many cheese-to-category

--- src/main/java/org/launchcode/models/data/Category.java
+++ src/main/java/org/launchcode/models/data/Category.java
@@ -1,20 +1,27 @@
 package org.launchcode.models.data;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import org.launchcode.models.Cheese;
+
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Category {
 
     @Id
-    @NotNull
+    @GeneratedValue
     private int id;
 
     @NotNull
     @Size(min=3, max=15)
     private String name;
+
+    @OneToMany
+    @JoinColumn(name="category_id")
+    private List<Cheese> cheeses = new ArrayList<>();
 
     public Category(){}
 
